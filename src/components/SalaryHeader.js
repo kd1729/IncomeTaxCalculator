9b6e060c3c1bab9172148d8143c4a5eda096bb05
onlykingKD <kaustubhdwivedi1729@gamil.com>
2023-01-19 00:17:19 +0530

    implemented Logic of coping values to below cells

--- src/components/SalaryHeader.js
+++ src/components/SalaryHeader.js
@@ -1,28 +1,29 @@
 export default function SalaryHeader() {
   return (
-    <div className="flex flex-row gap-8 text-lg font-semibold bg-gray-300 p-8">
-      <div className="px-4 py-2">Month</div>
-      <div className="px-4 py-2">Grade Pay</div>
+    <div className="flex flex-row gap-6 text-lg font-semibold bg-gray-300 py-4">
+      <div className="text-center bg-pink-100 px-4 py-2 w-24">Month</div>
+      <div className="text-center bg-pink-100 px-4 py-2 w-24">Grade Pay</div>
 
       {/*  positives */}
-      <div className="bg-green-200 px-4 py-2">Basic Pay</div>
-      <div className="bg-green-200 px-4 py-2">DA %</div>
-      <div className="bg-green-200 px-4 py-2">DA Amount</div>
-      <div className="bg-green-200 px-4 py-2">HRA</div>
-      <div className="bg-green-200 px-4 py-2">Bonus</div>
-      <div className="bg-green-200 px-4 py-2">Others</div>
-      <div className="bg-green-400 px-4 py-2">Total +</div>
+      <div className="text-center bg-green-200 px-4 py-2 w-24">Basic Pay</div>
+      <div className="text-center bg-green-200 px-4 py-2 w-24">DA %</div>
+      <div className="text-center bg-green-200 px-4 py-2 w-24">DA Amount</div>
+      <div className="text-center bg-green-200 px-4 py-2 w-24">HRA</div>
+      <div className="text-center bg-green-200 px-4 py-2 w-24">Bonus</div>
+      <div className="text-center bg-green-200 px-4 py-2 w-24">Others</div>
+      <div className="text-center bg-green-400 px-4 py-2 w-24">Total +</div>
 
       {/*  negatives */}
-      <div className="bg-red-200 px-4 py-2">NPS</div>
-      <div className="bg-red-200 px-4 py-2">GIS</div>
-      <div className="bg-red-200 px-4 py-2">TDS</div>
-      <div className="bg-red-400 px-4 py-2">Total -</div>
+      <div className="text-center bg-red-200 px-4 py-2 w-24">NPS</div>
+      <div className="text-center bg-red-200 px-4 py-2 w-24">PF</div>
+      <div className="text-center bg-red-200 px-4 py-2 w-24">GIS</div>
+      <div className="text-center bg-red-200 px-4 py-2 w-24">TDS</div>
+      <div className="text-center bg-red-400 px-4 py-2 w-24">Total -</div>
 
       {/*  total */}
-      <div className="px-4 py-2">Total</div>
+      <div className="text-center bg-pink-100 px-4 py-2 w-24">Total</div>
 
-      <div className="px-4 py-2">NPS Part2</div>
+      <div className="text-center bg-pink-100 px-4 py-2 w-24">NPS Part2</div>
     </div>
   );
 }
--- src/components/TotalSalaryComponent.js
+++ src/components/TotalSalaryComponent.js
@@ -1,10 +1,53 @@
+import { useEffect, useState, useRef } from "react";
+
 import SalaryHeader from "./SalaryHeader";
 
+const months = [
+  { id: 0, name: "January" },
+  { id: 1, name: "February" },
+  { id: 2, name: "March" },
+  { id: 3, name: "April" },
+  { id: 4, name: "May" },
+  { id: 5, name: "June" },
+  { id: 6, name: "July" },
+  { id: 7, name: "August" },
+  { id: 8, name: "September" },
+  { id: 9, name: "October" },
+  { id: 10, name: "November" },
+  { id: 11, name: "December" },
+];
+
 export default function TotalSalaryComponent() {
+  const [Basepay, setBasepay] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const CurrIdx = useRef(0);
+
   return (
-    // create a excel sheet with 14 columns and 14 rows
-    <div className="flex flex-col py-20 px-10">
-        <SalaryHeader />
+    <div className="flex flex-col">
+      <SalaryHeader />
+
+      {months.map((month) => (
+        <div className="flex flex-row gap-6 text-base font-semibold bg-gray-300">
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            {month.name}
+          </div>
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            <input
+              type="number"
+              className="w-24 text-center"
+              value={Basepay[month.id]}
+              onChange={(e) => {
+                CurrIdx.current = month.id;
+                Basepay[CurrIdx.current] = e.target.value;
+                for (let i = CurrIdx.current + 1; i < 12; i++) {
+                  Basepay[i] = e.target.value;
+                }
+
+                setBasepay([...Basepay]);
+              }}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
