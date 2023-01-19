477cd63ac884e16030d84736cd0f25aaa2e2e25d
onlykingKD <kaustubhdwivedi1729@gamil.com>
2023-01-19 12:52:28 +0530

    negative and total part done

--- src/components/TotalSalaryComponent.js
+++ src/components/TotalSalaryComponent.js
@@ -23,6 +23,8 @@ export default function TotalPositiveSalaryComponent() {
   ]);
   const CurrIdxGradepay = useRef(0);
 
+  // POSITIVE SALARY
+
   const [Basicpay, setBasicpay] = useState([
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   ]);
@@ -32,6 +34,14 @@ export default function TotalPositiveSalaryComponent() {
   const CurrIdxDAPerc = useRef(0);
 
   const [DA, setDA] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  useEffect(() => {
+    for (let i = 0; i < 12; i++) {
+      DA[i] = (parseFloat(Basicpay[i]) * parseFloat(DAPerc[i])) / 100;
+    }
+    setDA([...DA]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Basicpay, DAPerc]);
 
   const [HRA, setHRA] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   const CurrIdxHRA = useRef(0);
@@ -44,7 +54,9 @@ export default function TotalPositiveSalaryComponent() {
   ]);
   const CurrIdxOtherAllowance = useRef(0);
 
-  const [TotalPositiveSalary, setTotalPositiveSalary] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [TotalPositiveSalary, setTotalPositiveSalary] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
 
   useEffect(() => {
     for (let i = 0; i < 12; i++) {
@@ -57,8 +69,77 @@ export default function TotalPositiveSalaryComponent() {
     }
     setTotalPositiveSalary([...TotalPositiveSalary]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ Basicpay, DA, HRA, Bonus, OtherAllowance]);
+  }, [Basicpay, DA, HRA, Bonus, OtherAllowance]);
 
+  // NEGATIVE SALARY
+
+  const [NPS, setNPS] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  useEffect(() => {
+    for (let i = 0; i < 12; i++) {
+      NPS[i] = ( (parseFloat(Basicpay[i]) + parseFloat(DA[i])) * 10) / 100;
+
+    }
+    setNPS([...NPS]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Basicpay, DA]);
+
+  const [GPF, setGPF] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  useEffect(() => {
+    for (let i = 0; i < 12; i++) {
+      GPF[i] = (parseFloat(Basicpay[i]) * 10) / 100;
+    }
+    setGPF([...GPF]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Basicpay]);
+
+  const [GIS, setGIS] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const CurrIdxGIS = useRef(0);
+
+  const [TDS, setTDS] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const CurrIdxTDS = useRef(0);
+
+  const [TotalNegativeSalary, setTotalNegativeSalary] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+
+  useEffect(() => {
+    for (let i = 0; i < 12; i++) {
+      TotalNegativeSalary[i] =
+        parseFloat(NPS[i]) +
+        parseFloat(GPF[i]) +
+        parseFloat(GIS[i]) +
+        parseFloat(TDS[i]);
+    }
+    setTotalNegativeSalary([...TotalNegativeSalary]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [NPS, GPF, GIS, TDS]);
+
+  // TOTAL SALARY
+
+  const [NetSalary, setNetSalary] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+
+  useEffect(() => {
+    for (let i = 0; i < 12; i++) {
+      NetSalary[i] =
+        parseFloat(TotalPositiveSalary[i]) - parseFloat(TotalNegativeSalary[i]);
+    }
+    setNetSalary([...NetSalary]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [TotalPositiveSalary, TotalNegativeSalary]);
+
+  const [NPSByEmp, setNPSByEmp] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  useEffect(() => {
+    for (let i = 0; i < 12; i++) {
+      NPSByEmp[i] = ( (parseFloat(Basicpay[i]) + parseFloat(DA[i])) * 14) / 100;
+    }
+    setNPSByEmp([...NPSByEmp]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Basicpay, DA]);
 
   return (
     <div className="flex flex-col">
@@ -66,11 +147,10 @@ export default function TotalPositiveSalaryComponent() {
 
       {months.map((month) => (
         <div className="flex flex-row gap-6 text-base font-semibold bg-gray-300">
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
-            {month.name}
-          </div>
+          <div className="text-center py-2 my-2 w-24 ">{month.name}</div>
 
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+          {/* setting GradePay */}
+          <div className="text-center py-2 my-2 w-24 ">
             <input
               type="number"
               className="w-24 text-center"
@@ -86,7 +166,8 @@ export default function TotalPositiveSalaryComponent() {
             />
           </div>
 
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+          {/* setting BasicPay */}
+          <div className="text-center py-2 my-2 w-24 ">
             <input
               type="number"
               className="w-24 text-center"
@@ -102,7 +183,8 @@ export default function TotalPositiveSalaryComponent() {
             />
           </div>
 
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+          {/* setting DA% */}
+          <div className="text-center py-2 my-2 w-24 ">
             <input
               type="number"
               className="w-24 text-center"
@@ -110,22 +192,19 @@ export default function TotalPositiveSalaryComponent() {
               onChange={(e) => {
                 CurrIdxDAPerc.current = month.id;
                 DAPerc[CurrIdxDAPerc.current] = e.target.value;
-                DA[month.id] = (parseFloat(DAPerc[month.id]) * parseFloat(Basicpay[month.id])) / 100;
                 for (let i = CurrIdxDAPerc.current + 1; i < 12; i++) {
                   DAPerc[i] = e.target.value;
-                  DA[i] = (parseFloat(DAPerc[i]) * parseFloat(Basicpay[i])) / 100;
                 }
                 setDAPerc([...DAPerc]);
-                setDA([...DA]);
               }}
             />
           </div>
 
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
-            {DA[month.id]}
-          </div>
+          {/* displaying DA */}
+          <div className="text-center py-2 my-2 w-24 ">{Math.round(DA[month.id])}</div>
 
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+          {/* setting HRA */}
+          <div className="text-center py-2 my-2 w-24 ">
             <input
               type="number"
               className="w-24 text-center"
@@ -141,7 +220,8 @@ export default function TotalPositiveSalaryComponent() {
             />
           </div>
 
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+          {/* setting Bonus */}
+          <div className="text-center py-2 my-2 w-24 ">
             <input
               type="number"
               className="w-24 text-center"
@@ -157,7 +237,8 @@ export default function TotalPositiveSalaryComponent() {
             />
           </div>
 
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+          {/* setting OtherAllowance */}
+          <div className="text-center py-2 my-2 w-24 ">
             <input
               type="number"
               className="w-24 text-center"
@@ -173,10 +254,65 @@ export default function TotalPositiveSalaryComponent() {
             />
           </div>
 
-          <div className="text-center py-2 my-2 w-24 bg-slate-200">
-            {TotalPositiveSalary[month.id]}
+          {/* displaying TotalPositiveSalary */}
+          <div className="text-center py-2 my-2 w-24 ">
+            {Math.round(TotalPositiveSalary[month.id])}
           </div>
 
+          {/* displaying NPS */}
+          <div className="text-center py-2 my-2 w-24 ">{Math.round(NPS[month.id])}</div>
+
+          {/* displaying GPF */}
+          <div className="text-center py-2 my-2 w-24 ">{Math.round(GPF[month.id])}</div>
+
+          {/* setting GIS */}
+          <div className="text-center py-2 my-2 w-24 ">
+            <input
+              type="number"
+              className="w-24 text-center"
+              value={GIS[month.id]}
+              onChange={(e) => {
+                CurrIdxGIS.current = month.id;
+                GIS[CurrIdxGIS.current] = e.target.value;
+                for (let i = CurrIdxGIS.current + 1; i < 12; i++) {
+                  GIS[i] = e.target.value;
+                }
+                setGIS([...GIS]);
+              }}
+            />
+          </div>
+
+          {/* setting TDS */}
+          <div className="text-center py-2 my-2 w-24 ">
+            <input
+              type="number"
+              className="w-24 text-center"
+              value={TDS[month.id]}
+              onChange={(e) => {
+                CurrIdxTDS.current = month.id;
+                TDS[CurrIdxTDS.current] = e.target.value;
+                for (let i = CurrIdxTDS.current + 1; i < 12; i++) {
+                  TDS[i] = e.target.value;
+                }
+                setTDS([...TDS]);
+              }}
+            />
+          </div>
+
+          {/* displaying totalNegative */}
+          <div className="text-center py-2 my-2 w-24 ">
+            {Math.round(TotalNegativeSalary[month.id])}
+          </div>
+
+          {/* displaying NetSalary */}
+          <div className="text-center py-2 my-2 w-24 ">
+            {Math.round(NetSalary[month.id])}
+          </div>
+
+          {/* displaying NPS By Employer */}
+          <div className="text-center py-2 my-2 w-24 ">
+            {Math.round(NPSByEmp[month.id])}
+          </div>
         </div>
       ))}
     </div>
