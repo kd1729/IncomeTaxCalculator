d894410b201bbac2b65e6e1231a31b4cf4ff8734
onlykingKD <kaustubhdwivedi1729@gamil.com>
2023-01-19 11:59:39 +0530

    postive salary done

--- src/components/TotalSalaryComponent.js
+++ src/components/TotalSalaryComponent.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import SalaryHeader from "./SalaryHeader";
 
@@ -17,9 +17,48 @@ const months = [
   { id: 11, name: "December" },
 ];
 
-export default function TotalSalaryComponent() {
-  const [Basepay, setBasepay] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  const CurrIdx = useRef(0);
+export default function TotalPositiveSalaryComponent() {
+  const [Gradepay, setGradepay] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const CurrIdxGradepay = useRef(0);
+
+  const [Basicpay, setBasicpay] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const CurrIdxBasicpay = useRef(0);
+
+  const [DAPerc, setDAPerc] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const CurrIdxDAPerc = useRef(0);
+
+  const [DA, setDA] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const [HRA, setHRA] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const CurrIdxHRA = useRef(0);
+
+  const [Bonus, setBonus] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const CurrIdxBonus = useRef(0);
+
+  const [OtherAllowance, setOtherAllowance] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const CurrIdxOtherAllowance = useRef(0);
+
+  const [TotalPositiveSalary, setTotalPositiveSalary] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  useEffect(() => {
+    for (let i = 0; i < 12; i++) {
+      TotalPositiveSalary[i] =
+        parseFloat(Basicpay[i]) +
+        parseFloat(DA[i]) +
+        parseFloat(HRA[i]) +
+        parseFloat(Bonus[i]) +
+        parseFloat(OtherAllowance[i]);
+    }
+    setTotalPositiveSalary([...TotalPositiveSalary]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ Basicpay, DA, HRA, Bonus, OtherAllowance]);
+
 
   return (
     <div className="flex flex-col">
@@ -30,22 +69,114 @@ export default function TotalSalaryComponent() {
           <div className="text-center py-2 my-2 w-24 bg-slate-200">
             {month.name}
           </div>
+
           <div className="text-center py-2 my-2 w-24 bg-slate-200">
             <input
               type="number"
               className="w-24 text-center"
-              value={Basepay[month.id]}
+              value={Gradepay[month.id]}
               onChange={(e) => {
-                CurrIdx.current = month.id;
-                Basepay[CurrIdx.current] = e.target.value;
-                for (let i = CurrIdx.current + 1; i < 12; i++) {
-                  Basepay[i] = e.target.value;
+                CurrIdxGradepay.current = month.id;
+                Gradepay[CurrIdxGradepay.current] = e.target.value;
+                for (let i = CurrIdxGradepay.current + 1; i < 12; i++) {
+                  Gradepay[i] = e.target.value;
                 }
-
-                setBasepay([...Basepay]);
+                setGradepay([...Gradepay]);
               }}
             />
           </div>
+
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            <input
+              type="number"
+              className="w-24 text-center"
+              value={Basicpay[month.id]}
+              onChange={(e) => {
+                CurrIdxBasicpay.current = month.id;
+                Basicpay[CurrIdxBasicpay.current] = e.target.value;
+                for (let i = CurrIdxBasicpay.current + 1; i < 12; i++) {
+                  Basicpay[i] = e.target.value;
+                }
+                setBasicpay([...Basicpay]);
+              }}
+            />
+          </div>
+
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            <input
+              type="number"
+              className="w-24 text-center"
+              value={DAPerc[month.id]}
+              onChange={(e) => {
+                CurrIdxDAPerc.current = month.id;
+                DAPerc[CurrIdxDAPerc.current] = e.target.value;
+                DA[month.id] = (parseFloat(DAPerc[month.id]) * parseFloat(Basicpay[month.id])) / 100;
+                for (let i = CurrIdxDAPerc.current + 1; i < 12; i++) {
+                  DAPerc[i] = e.target.value;
+                  DA[i] = (parseFloat(DAPerc[i]) * parseFloat(Basicpay[i])) / 100;
+                }
+                setDAPerc([...DAPerc]);
+                setDA([...DA]);
+              }}
+            />
+          </div>
+
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            {DA[month.id]}
+          </div>
+
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            <input
+              type="number"
+              className="w-24 text-center"
+              value={HRA[month.id]}
+              onChange={(e) => {
+                CurrIdxHRA.current = month.id;
+                HRA[CurrIdxHRA.current] = e.target.value;
+                for (let i = CurrIdxHRA.current + 1; i < 12; i++) {
+                  HRA[i] = e.target.value;
+                }
+                setHRA([...HRA]);
+              }}
+            />
+          </div>
+
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            <input
+              type="number"
+              className="w-24 text-center"
+              value={Bonus[month.id]}
+              onChange={(e) => {
+                CurrIdxBonus.current = month.id;
+                Bonus[CurrIdxBonus.current] = e.target.value;
+                for (let i = CurrIdxBonus.current + 1; i < 12; i++) {
+                  Bonus[i] = e.target.value;
+                }
+                setBonus([...Bonus]);
+              }}
+            />
+          </div>
+
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            <input
+              type="number"
+              className="w-24 text-center"
+              value={OtherAllowance[month.id]}
+              onChange={(e) => {
+                CurrIdxOtherAllowance.current = month.id;
+                OtherAllowance[CurrIdxOtherAllowance.current] = e.target.value;
+                for (let i = CurrIdxOtherAllowance.current + 1; i < 12; i++) {
+                  OtherAllowance[i] = e.target.value;
+                }
+                setOtherAllowance([...OtherAllowance]);
+              }}
+            />
+          </div>
+
+          <div className="text-center py-2 my-2 w-24 bg-slate-200">
+            {TotalPositiveSalary[month.id]}
+          </div>
+
         </div>
       ))}
     </div>
