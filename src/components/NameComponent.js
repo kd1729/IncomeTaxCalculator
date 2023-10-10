686b2e831a9cb50eddf452e059dd322ea3f1fc66
Milind Palaria <lit2021047@iiitl.ac.in>
2023-10-10 22:12:14 +0530

    Website UI Enhanced

--- src/components/NameComponent.js
+++ src/components/NameComponent.js
@@ -1,10 +1,11 @@
-
 export default function NameComponent({ heading, profile, setProfile, value }) {
   return (
-    <div className="">
-      <div className=" w-60 text-lg  font-semibold pr-4 text-black">{heading} : </div>
+    <div className="teacher--class">
+      <span className="text-xl font-semibold pr-4 text-[#eeeeee] teacher-name--class">
+        {heading} :{" "}
+      </span>
       <input
-        className="pl-2 h-8 text-xl text-blue-800 outline-none border-2 border-slate-600"
+        className="pl-2 h-8 text-xl outline-none label--class"
         type="text"
         value={profile[value]}
         onChange={(e) => setProfile({ ...profile, [value]: e.target.value })}
